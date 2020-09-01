46ea3dba0d5947bd54ae8d61afdbdf08893baf2f
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-31 17:51:11 -0700

    setup template for all components

---
+++ src/components/work/noteworthy/Noteworthy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Noteworthy = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Noteworthy
---
+++ src/components/work/noteworthy/NoteworthyProjects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteworthyProjects = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NoteworthyProjects
