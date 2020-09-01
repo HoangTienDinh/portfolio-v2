9787384eeeb2e39f4c6337b5c6f3a924c2df9d07
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-01 12:44:46 -0700

    start of layout comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
 }
 
 export default App;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from './Header'
+import Social from './Social'
+import Footer from './Footer'
+
 
 const Layout = () => {
     return (
         <div>
-            
+            <Header />
+            <Social />
+            <Footer />
         </div>
     )
 }
