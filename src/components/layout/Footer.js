5e74621d5b885c6ac9458ce9e8e3b5fe4385303f
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-01 14:47:16 -0700

    center footer with link at bottom of the page

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Typography, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    bottom: 0,
+    left: 0,
+    position: 'fixed',
+    width: '100%'
+  },
+}));
 
 const Footer = () => {
-    return (
-        <div>
-            FOOTER
-        </div>
-    )
-}
+  const classes = useStyles();
 
-export default Footer
+  return (
+    <div className={classes.root}>
+      <Link href="https://github.com/HoangTienDinh/portfolio-v2" target="blank">
+        <Typography>Designed & built by Hoang Dinh</Typography>
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
