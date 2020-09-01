aa1885631afca693e7e7958baf211212b6627e5c
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-01 13:46:39 -0700

    install icons, add icons with relevant links

--- src/components/layout/Social.js
+++ src/components/layout/Social.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import EmailIcon from "@material-ui/icons/Email";
+import Link from "@material-ui/core/Link";
 
 const Social = () => {
-    return (
-        <div>
-            SOCIAL
-        </div>
-    )
-}
+  return (
+    <div>
+      <Link href="https://github.com/HoangTienDinh" target="blank">
+        <GitHubIcon />
+      </Link>
+      <Link href="https://www.linkedin.com/in/hoangdinh90/" target="blank">
+        <LinkedInIcon />
+      </Link>
+      <Link href="https://www.instagram.com/hohohoang/" target="blank">
+        <InstagramIcon />
+      </Link>
+      <Link href="mailto:hoangdinh90@gmail.com" target="blank">
+        <EmailIcon />
+      </Link>
+    </div>
+  );
+};
 
-export default Social
+export default Social;
