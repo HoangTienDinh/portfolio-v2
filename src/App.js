9f9a4f4163292c3c114e86169e7d7889777d4f83
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-02 20:05:51 -0700

    look at how the page looks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Layout from "./components/layout/Layout";
 import Landing from "./components/Landing";
+import About from "./components/About";
 import Work from "./components/work/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Layout />
       <Landing />
+      <About />
       <Work />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Landing from './Landing'
 
 const Contact = () => {
     return (
         <div>
-            
+            <Landing />
         </div>
     )
 }
