adaa7d770099d5d89e83ac2ffaf66752289edfb4
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-03 21:53:07 -0700

    add dummy info

--- src/data/work.js
+++ src/data/work.js
@@ -1,56 +1,56 @@
 const portfolio = {
-    featureOne: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    featureTwo: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    featureThree: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthyOne: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthyTwo: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthyThree: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthyFour: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthyFive: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-    noteworthySix: {
-        title: '',
-        description: '',
-        image: '',
-        technology: [],
-    },
-}
+  featureOne: {
+    title: "Test One",
+    description: "Here is some information about Test One, my best Test",
+    image: "dummy data, i'll figure out dummy photos soon",
+    technology: ['React', 'Javascript', 'MUI'],
+  },
+  featureTwo: {
+    title: "Test Two",
+    description: "Sure here is some more dummy information, but it's on TEST TWO",
+    image: "another image",
+    technology: ['React', 'Firebase', 'Tailwind'],
+  },
+  featureThree: {
+    title: "TEST THREE",
+    description: "OMG IT'S ONE OF THE TOP THREEEEEEEEEEEEEE",
+    image: "Hi",
+    technology: ['Javascript', 'Alan AI', 'Firebase'],
+  },
+  noteworthyOne: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+  noteworthyTwo: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+  noteworthyThree: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+  noteworthyFour: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+  noteworthyFive: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+  noteworthySix: {
+    title: "",
+    description: "",
+    image: "",
+    technology: [],
+  },
+};
