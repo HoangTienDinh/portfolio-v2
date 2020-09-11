9680e4c6478ff9884f1cfbddd24c1d654e4d47cc
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 16:50:52 -0700

    style text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CardMedia from "@material-ui/core/CardMedia";
-import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
 import Image from "../assets/newWest.jpg";
 import Typography from "@material-ui/core/Typography";
@@ -8,11 +7,16 @@ import { Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: 'inherit'
+    width: "inherit",
   },
   media: {
     height: "100vh",
-    width: "inherit",
+  },
+  headline: {
+    position: "absolute",
+    color: "white",
+    top: "45%",
+    left: "5%",
   },
 }));
 
@@ -20,11 +24,13 @@ const Landing = () => {
   const classes = useStyles();
 
   return (
-      <Box className={classes.root}>
-        <CardMedia className={classes.media} image={Image} title="Landing Page">
-          <Typography>Hello</Typography>
-        </CardMedia>
-      </Box>
+    <Box className={classes.root}>
+      <CardMedia className={classes.media} image={Image} title="Landing Page">
+        <Typography className={classes.headline} variant="h3">
+          Web Developer
+        </Typography>
+      </CardMedia>
+    </Box>
   );
 };
 
