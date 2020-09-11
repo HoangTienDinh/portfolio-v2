d28483f1f3eafa4b88c44cfed67cd0fc57576e44
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 14:58:25 -0700

    import MUI comps

--- src/components/work/NoteworthyProjects.js
+++ src/components/work/NoteworthyProjects.js
@@ -1,18 +1,29 @@
 import React from "react";
 import data from "../../data/data";
 import Reveal from "react-reveal";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles(() => ({
+  root: {},
+  container: {
+    width: "25vw",
+    height: "25vh",
+  },
+}));
 const { noteworthyProj } = data.portfolio;
 
 const NoteworthyProjects = () => {
   return (
-    <div>
+    <Container>
+      <Typography variant='h3' align='center'>Noteworthy Projects</Typography>
       {noteworthyProj.map((project, i) => (
         <Reveal duration={i * 1000} effect="fadeInUp">
-          <div key={i}>{project.title}</div>
+          <Grid container>
+            <Grid item></Grid>
+          </Grid>
         </Reveal>
       ))}
-    </div>
+    </Container>
   );
 };
 
