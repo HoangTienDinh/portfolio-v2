e637ca97f1b3caeb06c104317e93aedefbdc0f10
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-10 23:38:35 -0700

    import data

--- src/components/work/NoteworthyProjects.js
+++ src/components/work/NoteworthyProjects.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import data from '../../data/data';
+
+const { noteworthyProj } = data.portfolio
 
 const NoteworthyProjects = () => {
     return (
         <div>
-            
+            {noteworthyProj.map((project, i) => (
+                <div key={i}>{project.title}</div>
+            ))}
         </div>
     )
 }
