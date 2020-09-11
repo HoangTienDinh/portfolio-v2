b9ceb8f038e33d9ec02581543061d169b7941c08
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 15:23:10 -0700

    render data and add css

--- src/components/work/NoteworthyProjects.js
+++ src/components/work/NoteworthyProjects.js
@@ -3,26 +3,54 @@ import data from "../../data/data";
 import Reveal from "react-reveal";
 import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 
-const useStyles = makeStyles(() => ({
-  root: {},
-  container: {
-    width: "25vw",
-    height: "25vh",
-  },
-}));
 const { noteworthyProj } = data.portfolio;
 
+const useStyles = makeStyles(() => ({
+  root: {
+    height: "100%",
+    width: "100%",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+    gridGap: '15px',
+    position: "relative",
+    marginTop: "50px",
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+}));
+
 const NoteworthyProjects = () => {
+  const classes = useStyles();
+
   return (
-    <Container>
-      <Typography variant='h3' align='center'>Noteworthy Projects</Typography>
-      {noteworthyProj.map((project, i) => (
-        <Reveal duration={i * 1000} effect="fadeInUp">
-          <Grid container>
-            <Grid item></Grid>
-          </Grid>
-        </Reveal>
-      ))}
+    <Container className={classes.root}>
+      <Typography variant="h3" align="center">
+        Noteworthy Projects
+      </Typography>
+      <Grid
+        className={classes.container}
+        container
+        justify="space-evenly"
+        alignItems="center"
+
+      >
+        {noteworthyProj.map((project, i) => (
+          <Reveal duration={i * 1000} effect="fadeInUp">
+            <Grid className={classes.grid} item >
+              <Typography>{project.title}</Typography>
+              <Typography>{project.description}</Typography>
+              {project.technology.map(tech => (
+                <Typography>{tech}</Typography>
+              ))}
+            </Grid>
+          </Reveal>
+        ))}
+      </Grid>
     </Container>
   );
 };
