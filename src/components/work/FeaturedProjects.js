4568940db4ecbdd0f7cc80818111420847a8a691
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-10 22:29:21 -0700

    render test data

--- src/components/work/FeaturedProjects.js
+++ src/components/work/FeaturedProjects.js
@@ -1,8 +1,19 @@
 import React from "react";
 import data from "../../data/data";
 
+const { featuredProj } = data.portfolio;
+
 const FeaturedProjects = () => {
-  return <div></div>;
+  return (
+    <div>
+      {featuredProj.map((project, i) => (
+        <div key={i}>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FeaturedProjects;
