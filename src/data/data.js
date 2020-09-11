85298588bba2afd1863c5d147ed4b50deb70206c
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-10 22:24:42 -0700

    change data structure into an array of objects for projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,59 +1,63 @@
 const portfolio = {
-  featureOne: {
-    title: "Test One",
-    description: "Here is some information about Test One, my best Test",
-    image: "dummy data, i'll figure out dummy photos soon",
-    technology: ["React", "Javascript", "MUI"],
-  },
-  featureTwo: {
-    title: "Test Two",
-    description:
-      "Sure here is some more dummy information, but it's on TEST TWO",
-    image: "another image",
-    technology: ["React", "Firebase", "Tailwind"],
-  },
-  featureThree: {
-    title: "TEST THREE",
-    description: "OMG IT'S ONE OF THE TOP THREEEEEEEEEEEEEE",
-    image: "Hi",
-    technology: ["Javascript", "Alan AI", "Firebase"],
-  },
-  noteworthyOne: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
-  noteworthyTwo: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
-  noteworthyThree: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
-  noteworthyFour: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
-  noteworthyFive: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
-  noteworthySix: {
-    title: "",
-    description: "",
-    image: "",
-    technology: [],
-  },
+  featuredProject: [
+    {
+      title: "Test One",
+      description: "Here is some information about Test One, my best Test",
+      image: "dummy data, i'll figure out dummy photos soon",
+      technology: ["React", "Javascript", "MUI"],
+    },
+    {
+      title: "Test Two",
+      description:
+        "Sure here is some more dummy information, but it's on TEST TWO",
+      image: "another image",
+      technology: ["React", "Firebase", "Tailwind"],
+    },
+    {
+      title: "TEST THREE",
+      description: "OMG IT'S ONE OF THE TOP THREEEEEEEEEEEEEE",
+      image: "Hi",
+      technology: ["Javascript", "Alan AI", "Firebase"],
+    },
+  ],
+  noteworthyProjects: [
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+    {
+      title: "",
+      description: "",
+      image: "",
+      technology: [],
+    },
+  ],
   aboutMe: {
     headline: "A Little Bit About Me",
     description: "Here's some info that I think makes me sounds cool",
