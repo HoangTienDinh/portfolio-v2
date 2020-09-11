d2950449b2e349ff306a646c61c76ae56ef8d831
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 09:47:42 -0700

    add revel comp

--- src/components/work/NoteworthyProjects.js
+++ src/components/work/NoteworthyProjects.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import data from '../../data/data';
+import React from "react";
+import data from "../../data/data";
+import Reveal from "react-reveal";
 
-const { noteworthyProj } = data.portfolio
+const { noteworthyProj } = data.portfolio;
 
 const NoteworthyProjects = () => {
-    return (
-        <div>
-            {noteworthyProj.map((project, i) => (
-                <div key={i}>{project.title}</div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      {noteworthyProj.map((project, i) => (
+        <Reveal duration={i * 1000} effect="fadeInUp">
+          <div key={i}>{project.title}</div>
+        </Reveal>
+      ))}
+    </div>
+  );
+};
 
-export default NoteworthyProjects
+export default NoteworthyProjects;
