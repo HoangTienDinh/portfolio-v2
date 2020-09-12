f6b7cce1a335904f6ec416bab2522164329de856
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-12 12:40:05 -0700

    refine method to conditionally render css

--- src/components/work/FeaturedProjects.js
+++ src/components/work/FeaturedProjects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Grid,
   CardMedia,
@@ -16,7 +16,7 @@ const useStyles = makeStyles(() => ({
   root: {
     maxWidth: 345,
   },
-  gridLeft: {
+  projectContainer: {
     display: "grid",
     gap: "10px",
     gridTemplateColumns: "repeat(12, 1fr)",
@@ -31,16 +31,13 @@ const useStyles = makeStyles(() => ({
     height: 0,
     paddingTop: "56.25%",
   },
-  title: {
-    textAlign: (side) => (side ? "right" : "left"),
-  },
-  content: {
+  contentLeft: {
     gridColumn: "7 / -1",
     textAlign: "right",
     gridArea: "1 / 1 / -1 / 7",
     position: "relative",
   },
-  image: {
+  imageLeft: {
     gridColumn: "1 / 8",
     boxShadow: "0 10px 30px -15px black",
     transition: "var(--transtion)",
@@ -51,60 +48,32 @@ const useStyles = makeStyles(() => ({
 }));
 
 const FeaturedProjects = () => {
-  const [side, setSide] = useState(true);
-  const classes = useStyles(side);
+  const classes = useStyles();
   const preventDefault = (event) => event.preventDefault();
 
   // write a conditional using i % 2 in className for the element to render it left or right side
   const handleSide = (project, index) => {
+    let contentContainer;
+    let imageContainer;
+
+
+
     if (index === 0 || index % 2 === 0) {
-      // HOW DO I HANDLE STATE AND FLIP !SIDE TO RENDER THE CORRECT CSS
-
-      return (
-        <Grid Container key={index} className={classes.gridLeft}>
-          <Grid item className={classes.image}>
-            <Link
-              href={project.website ? project.website : project.github}
-              onClick={preventDefault}
-            >
-              <CardMedia
-                className={classes.media}
-                image={project.image}
-                title={project.title}
-              />
-            </Link>
-          </Grid>
-          <Grid item className={classes.content}>
-            <Link
-              href={project.website ? project.website : project.github}
-              onClick={preventDefault}
-            >
-              <Typography className={classes.title}>{project.title}</Typography>
-            </Link>
-            <Typography>{project.description}</Typography>
-            {project.technology.map((tech, i) => (
-              <Typography key={i}>{tech}</Typography>
-            ))}
-          </Grid>
-        </Grid>
-      );
+      contentContainer = classes.contentLeft
+      imageContainer = classes.imageLeft
     } else {
-      return (
-        <Grid Container key={index} className={classes.right}>
-          <Grid item>
-            <Link
-              href={project.website ? project.website : project.github}
-              onClick={preventDefault}
-            >
-              <Typography className={classes.title}>{project.title}</Typography>
-            </Link>
-            <Typography>{project.description}</Typography>
-            {project.technology.map((tech, i) => (
-              <Typography key={i}>{tech}</Typography>
-            ))}
-          </Grid>
+        contentContainer = classes.contentRight
+        imageContainer = classes.imageRight
+    }
 
-          <Grid item>
+    // if (index === 0 || index % 2 === 0) {
+
+    //   contentContainer = classes.contentLeft
+    //   imageContainer = classes.imageLeft
+
+      return (
+        <Grid Container key={index} className={classes.projectContainer}>
+          <Grid item className={imageContainer}>
             <Link
               href={project.website ? project.website : project.github}
               onClick={preventDefault}
@@ -116,9 +85,55 @@ const FeaturedProjects = () => {
               />
             </Link>
           </Grid>
+          <Grid item className={contentContainer}>
+            <Link
+              href={project.website ? project.website : project.github}
+              onClick={preventDefault}
+            >
+              <Typography>{project.title}</Typography>
+            </Link>
+            <Typography>{project.description}</Typography>
+            {project.technology.map((tech, i) => (
+              <Typography key={i}>{tech}</Typography>
+            ))}
+          </Grid>
         </Grid>
       );
-    }
+    // } 
+    
+    // else {
+    //   return (
+    //     <Grid Container key={index} className={classes.right}>
+    //       <Grid item>
+    //         <Link
+    //           href={project.website ? project.website : project.github}
+    //           onClick={preventDefault}
+    //         >
+    //           <Typography>{project.title}</Typography>
+    //         </Link>
+    //         <Typography>{project.description}</Typography>
+    //         {project.technology.map((tech, i) => (
+    //           <Typography key={i}>{tech}</Typography>
+    //         ))}
+    //       </Grid>
+
+    //       <Grid item>
+    //         <Link
+    //           href={project.website ? project.website : project.github}
+    //           onClick={preventDefault}
+    //         >
+    //           <CardMedia
+    //             className={classes.media}
+    //             image={project.image}
+    //             title={project.title}
+    //           />
+    //         </Link>
+    //       </Grid>
+    //     </Grid>
+    //   );
+    // }
+
+
   };
 
   return (
