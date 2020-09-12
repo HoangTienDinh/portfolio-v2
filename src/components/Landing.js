4a0499a9fa52093beec860cbb52afca36ac9d020
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 17:06:54 -0700

    add Typist animation comp

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Image from "../assets/newWest.jpg";
 import Typography from "@material-ui/core/Typography";
 import { Box } from "@material-ui/core";
+import Typist from "react-typist";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,7 +16,13 @@ const useStyles = makeStyles((theme) => ({
   headline: {
     position: "absolute",
     color: "white",
-    top: "45%",
+    top: "50%",
+    left: "5%",
+  },
+  name: {
+    position: "absolute",
+    color: "white",
+    top: "40%",
     left: "5%",
   },
 }));
@@ -26,9 +33,19 @@ const Landing = () => {
   return (
     <Box className={classes.root}>
       <CardMedia className={classes.media} image={Image} title="Landing Page">
-        <Typography className={classes.headline} variant="h3">
-          Web Developer
-        </Typography>
+        <Typist>
+          <Typography className={classes.name} variant="h3">
+            Hoang Dinh
+          </Typography>
+        <Typist.Delay ms={200} />
+          <Typography className={classes.headline} variant="h3">
+            Photographer
+          </Typography>
+          <Typist.Backspace count={12} delay={200} />
+          <Typography className={classes.headline} variant="h3">
+            Web Developer
+          </Typography>
+        </Typist>
       </CardMedia>
     </Box>
   );
