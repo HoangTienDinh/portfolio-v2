fbd3742ee89dda7f9e3c74451a574e22cca734c4
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-11 17:11:32 -0700

    add general layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,40 @@
 import React from "react";
-import Landing from "./Landing";
+import Image from "../assets/newWest.jpg";
+import { Box, CardMedia } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "inherit",
+  },
+  media: {
+    height: "100vh",
+  },
+  headline: {
+    position: "absolute",
+    color: "white",
+    top: "50%",
+    left: "5%",
+  },
+  name: {
+    position: "absolute",
+    color: "white",
+    top: "40%",
+    left: "5%",
+  },
+}));
 
 const Contact = () => {
+  const classes = useStyles();
+
   return (
-    <div id="contact">
-      <Landing />
-    </div>
+    <Box className={classes.root} id="contact">
+      <CardMedia
+        className={classes.media}
+        image={Image}
+        title="Landing Page"
+      ></CardMedia>
+    </Box>
   );
 };
 
