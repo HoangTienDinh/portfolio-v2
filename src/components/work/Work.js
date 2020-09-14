ab18163f6b1cc0bb4bdb6e30fdd3c3b7a777303e
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-14 15:23:07 -0700

    add color scheme

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,16 +1,22 @@
 import React from "react";
 import FeaturedProjects from "./FeaturedProjects";
 import NoteworthyProjects from "./NoteworthyProjects";
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, makeStyles } from "@material-ui/core";
 import Reveal from "react-reveal/Reveal";
 
+const useStyles = makeStyles(() => ({
+  title: {
+    color: '#cccccc'
+  },
+}))
 const Work = () => {
+  const classes = useStyles();
 
   return (
     <Container id="work">
       <Reveal duration={2000} effect="fadeInUp">
         <Container>
-          <Typography variant="h3">Some Things I've Built. </Typography>
+          <Typography className={classes.title} variant="h3">Some Things I've Built. </Typography>
         </Container>
       </Reveal>
       <FeaturedProjects />
