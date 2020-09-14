76d4bbe5a12855e75a48a4bbe3e938937c02d5bb
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-14 13:53:41 -0700

    add header for work section

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,11 +1,17 @@
 import React from "react";
 import FeaturedProjects from "./FeaturedProjects";
 import NoteworthyProjects from "./NoteworthyProjects";
-import { Container } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
+import Reveal from "react-reveal/Reveal";
 
 const Work = () => {
   return (
     <Container id="work">
+      <Reveal duration="2000" effect="fadeInUp">
+        <Container>
+          <Typography variant="h3">Some Things I've Built. </Typography>
+        </Container>
+      </Reveal>
       <FeaturedProjects />
       <NoteworthyProjects />
     </Container>
