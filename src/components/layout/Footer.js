12294bac45d39172390476d1fd6c70af354a5b19
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-15 13:13:00 -0700

    npm i react-scroll

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,6 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import EmailIcon from "@material-ui/icons/Email";
-import { FullscreenExit } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
