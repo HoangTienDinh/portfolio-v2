fe23e68ede0c27cbec1d498ce0c1f30dfce7b62f
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-14 17:09:50 -0700

    small mui change

--- src/components/work/NoteworthyProjects.js
+++ src/components/work/NoteworthyProjects.js
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   noteworthy: {
     color: "#cccccc",
     textAlign: "center",
+    marginBottom: theme.spacing(5),
   },
   project: {
     border: "solid 1px black",
@@ -58,63 +59,64 @@ const NoteworthyProjects = () => {
         </Typography>
       </Reveal>
 
-      <Grid container spacing={3}>
+      <Grid container justify="center" spacing={4}>
         {noteworthyProj.map((project, i) => (
-          <Grid
-            container
-            item
-            xs={12}
-            md={8}
-            lg={4}
-            direction='column'
-            className={classes.project}
-          >
-            <Reveal duration={i * 500} effect="fadeInUp">
-              <Grid container item justify="space-between">
-                <FolderIcon />
-                <Grid item>
-                  {project.github && (
-                    <Link
-                      className={classes.links}
-                      href={project.github}
-                      target="blank"
-                    >
-                      <GitHubIcon />
-                    </Link>
-                  )}
-                  {project.website && (
-                    <Link
-                      className={classes.links}
-                      href={project.website}
-                      target="blank"
-                    >
-                      <OpenInNewIcon />
-                    </Link>
-                  )}
-                </Grid>
-              </Grid>
-
-              <Grid item>
-                <Typography className={classes.title} variant="h6">
-                  {project.title}
-                </Typography>
-              </Grid>
-
-              <Grid item>
-                <Typography className={classes.description}>
-                  {project.description}
-                </Typography>
-              </Grid>
-
-              <Grid container>
-                {project.technology.map((tech, i) => (
-                  <Grid item key={i} className={classes.technology}>
-                    <Typography>{tech}</Typography>
+            <Grid
+              container
+              item
+              xs={12}
+              sm={8}
+              md={8}
+              lg={4}
+              direction="column"
+              className={classes.project}
+            >
+              <Reveal duration={i * 500} effect="fadeInUp">
+                <Grid container item justify="space-between">
+                  <FolderIcon />
+                  <Grid item>
+                    {project.github && (
+                      <Link
+                        className={classes.links}
+                        href={project.github}
+                        target="blank"
+                      >
+                        <GitHubIcon />
+                      </Link>
+                    )}
+                    {project.website && (
+                      <Link
+                        className={classes.links}
+                        href={project.website}
+                        target="blank"
+                      >
+                        <OpenInNewIcon />
+                      </Link>
+                    )}
                   </Grid>
-                ))}
-              </Grid>
-            </Reveal>
-          </Grid>
+                </Grid>
+
+                <Grid item>
+                  <Typography className={classes.title} variant="h6">
+                    {project.title}
+                  </Typography>
+                </Grid>
+
+                <Grid item>
+                  <Typography className={classes.description}>
+                    {project.description}
+                  </Typography>
+                </Grid>
+
+                <Grid container>
+                  {project.technology.map((tech, i) => (
+                    <Grid item key={i} className={classes.technology}>
+                      <Typography>{tech}</Typography>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Reveal>
+            </Grid>
         ))}
       </Grid>
     </Container>
