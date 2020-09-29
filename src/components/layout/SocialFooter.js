345ed491adfcc692e75353ddea52a9847577e596
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-29 12:19:30 -0700

    start of new comp

---
+++ src/components/layout/SocialFooter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import EmailIcon from "@material-ui/icons/Email";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Link } from "@material-ui/core";
+
+import data from "../../data/data";
+
+const { social } = data.portfolio;
+
+const SocialFooter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SocialFooter
