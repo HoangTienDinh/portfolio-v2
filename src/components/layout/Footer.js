ec6e7f0a04b3e33e50374f6f7ad42f270da589f7
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-29 12:34:36 -0700

    remove icons from footer, make new comp to put them in

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Typography, Link, Hidden, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import EmailIcon from "@material-ui/icons/Email";
+import SocialFooter from './SocialFooter';
 
 import data from "../../data/data";
 
@@ -50,38 +47,8 @@ const Footer = () => {
       alignContent="center"
       className={classes.root}
     >
-      <Hidden only={["lg", "xl"]}>
-        <Grid item className={classes.icons}>
-          <Link
-            className={classes.links}
-            href={social.github}
-            target="_blank"
-          >
-            <GitHubIcon />
-          </Link>
-          <Link
-            className={classes.links}
-            href={social.linkedIn}
-            target="_blank"
-          >
-            <LinkedInIcon />
-          </Link>
-          <Link
-            className={classes.links}
-            href={social.instagram}
-            target="_blank"
-          >
-            <InstagramIcon />
-          </Link>
-          <Link
-            className={classes.links}
-            href={social.email}
-            target="_blank"
-          >
-            <EmailIcon />
-          </Link>
-        </Grid>
-      </Hidden>
+      <SocialFooter />
+      
       <Grid item>
         <Link
           href="https://github.com/HoangTienDinh/portfolio-v2"
--- src/components/layout/SocialFooter.js
+++ src/components/layout/SocialFooter.js
@@ -10,12 +10,40 @@ import data from "../../data/data";
 
 const { social } = data.portfolio;
 
-const SocialFooter = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
-export default SocialFooter
+const SocialFooter = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root} justify="center" spacing={10}>
+      <Grid item>
+        <Link href={social.github} target="_blank">
+          <GitHubIcon />
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link href={social.linkedIn} target="_blank">
+          <LinkedInIcon />
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link href={social.instagram} target="_blank">
+          <InstagramIcon />
+        </Link>
+      </Grid>
+
+      <Grid item>
+        <Link href={social.email} target="_blank">
+          <EmailIcon />
+        </Link>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SocialFooter;
